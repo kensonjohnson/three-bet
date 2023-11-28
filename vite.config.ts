af580beacd0e4ecc8a205f426c584a7289796b9e
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-27 21:02:46 -0600

    Added examples for server.proxy

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,17 @@ export default {
       input: globSync(resolve(cwd(), "frontend", "**/*.html")),
     },
   },
+  server: {
+    proxy: {
+      // Example proxying to a local server
+      // "/api": {
+      //   target: "http://localhost:3000",
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ""),
+      // },
+
+      // Shorthand for proxying to specific endpoint
+      "/hello": "http://localhost:3000",
+    },
+  },
 };
