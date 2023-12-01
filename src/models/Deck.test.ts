c1d11c1c6ca643fa4a1107cacb0adc68fc76c950
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2023-12-01 13:15:14 -0500

    Updated unit tests to for private properties.

--- src/models/Deck.test.ts
+++ src/models/Deck.test.ts
@@ -5,11 +5,11 @@ describe("Deck Model", () => {
 
   describe("constructor", () => {
     it("should create a deck of cards", () => {
-      expect(deck.cards.length).toEqual(52);
+      expect(deck.deckSize).toEqual(52);
     });
 
     it("should create a discard pile", () => {
-      expect(deck.discardPile.length).toEqual(0);
+      expect(deck.discardPileSize).toEqual(0);
     });
   });
 
@@ -17,13 +17,13 @@ describe("Deck Model", () => {
     describe("burnOneCard", () => {
       it("should burn a card", () => {
         deck.burnOneCard();
-        expect(deck.cards.length).toEqual(51);
-        expect(deck.discardPile.length).toEqual(1);
+        expect(deck.deckSize).toEqual(51);
+        expect(deck.discardPileSize).toEqual(1);
       });
 
       it("should return false if there are no cards to burn", () => {
         const deck = new Deck();
-        deck.cards = [];
+        deck.dealManyCards(deck.deckSize);
         expect(deck.burnOneCard()).toEqual(false);
       });
     });
@@ -31,14 +31,14 @@ describe("Deck Model", () => {
     describe("dealOneCard", () => {
       it("should deal a card", () => {
         const card = deck.dealOneCard();
-        expect(deck.cards.length).toEqual(50);
-        expect(deck.discardPile.length).toEqual(1);
+        expect(deck.deckSize).toEqual(50);
+        expect(deck.discardPileSize).toEqual(1);
         expect(card).toBeDefined();
       });
 
       it("should return null if there are no cards to deal", () => {
         const deck = new Deck();
-        deck.cards = [];
+        deck.dealManyCards(deck.deckSize);
         expect(deck.dealOneCard()).toEqual(null);
       });
     });
@@ -46,15 +46,15 @@ describe("Deck Model", () => {
     describe("dealTwoCards", () => {
       it("should deal two cards", () => {
         const cards = deck.dealTwoCards();
-        expect(deck.cards.length).toEqual(48);
-        expect(deck.discardPile.length).toEqual(1);
+        expect(deck.deckSize).toEqual(48);
+        expect(deck.discardPileSize).toEqual(1);
         expect(cards).toBeDefined();
         expect(cards!.length).toEqual(2);
       });
 
       it("should return null if there are no cards to deal", () => {
         const deck = new Deck();
-        deck.cards = [];
+        deck.dealManyCards(deck.deckSize);
         expect(deck.dealTwoCards()).toEqual(null);
       });
     });
@@ -62,15 +62,15 @@ describe("Deck Model", () => {
     describe("dealThreeCards", () => {
       it("should deal three cards", () => {
         const cards = deck.dealThreeCards();
-        expect(deck.cards.length).toEqual(45);
-        expect(deck.discardPile.length).toEqual(1);
+        expect(deck.deckSize).toEqual(45);
+        expect(deck.discardPileSize).toEqual(1);
         expect(cards).toBeDefined();
         expect(cards!.length).toEqual(3);
       });
 
       it("should return null if there are no cards to deal", () => {
         const deck = new Deck();
-        deck.cards = [];
+        deck.dealManyCards(deck.deckSize);
         expect(deck.dealThreeCards()).toEqual(null);
       });
     });
@@ -78,15 +78,15 @@ describe("Deck Model", () => {
     describe("dealManyCards", () => {
       it("should deal many cards", () => {
         const cards = deck.dealManyCards(7);
-        expect(deck.cards.length).toEqual(38);
-        expect(deck.discardPile.length).toEqual(1);
+        expect(deck.deckSize).toEqual(38);
+        expect(deck.discardPileSize).toEqual(1);
         expect(cards).toBeDefined();
         expect(cards!.length).toEqual(7);
       });
 
       it("should return null if there are no cards to deal", () => {
         const deck = new Deck();
-        deck.cards = [];
+        deck.dealManyCards(deck.deckSize);
         expect(deck.dealManyCards(7)).toEqual(null);
       });
     });
